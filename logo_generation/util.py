27a4b72a4330a6ace5bd6cd976946bd97d59a08d
송지민_T6088 <re-ming@naver.com>
2024-03-04 16:05:44 +0900

    Feat: add util - kor label to eng label (#20)
    * Feat: add util
    
    #18

---
+++ logo_generation/util.py
@@ -0,0 +1,27 @@
+import os
+from googletrans import Translator
+from tqdm import tqdm
+
+    
+def label_ko2en():
+    label_path = '/data/ephemeral/home/dataset/label/'
+    en_label_path = '/data/ephemeral/home/dataset/en_label/'
+    if not os.path.exists(en_label_path): os.makedirs(en_label_path)
+    
+    label_list = os.listdir(label_path)
+    i = 0
+    for label in tqdm(label_list):
+        if not i%40:
+            translator = Translator()
+        with open(label_path + label, 'r') as f:
+            txt = f.read().strip()
+        res = translator.translate(txt, src='ko', dest='en')
+        with open(en_label_path + label, 'w') as f:
+            f.write(res.text)
+        i += 1
+    
+    
+if __name__ == "__main__":
+    label_ko2en()
+    
+    
