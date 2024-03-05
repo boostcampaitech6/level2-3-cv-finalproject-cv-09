e30831c56d15fdd9ecc6b709e4b086f026109059
sharkmeal <qkr99102@naver.com>
2024-03-05 17:45:49 +0900

    Fix: page/modeselect desing fix
    #19

--- frontend/src/pages/ModeSelect.js
+++ frontend/src/pages/ModeSelect.js
@@ -17,27 +17,22 @@ const ModeSelect = () =>{
 return(
     <div><MainNav/>
 <div className='container' style={{paddingTop: "100px", paddingBottom: "200px"}}>
-<div className="QuestionT1">{name}(이)라는 로고를 만들게요.</div>
+<div className="nametext" >{name}</div>
+<div className="QuestionT2">라는 로고를 만들게요.</div>
 <div className="QuestionT2">원하시는 생성 방식을 선택해주세요.</div>
-<div >
-  <ul className="button_list">
-  <li className="li">
-    <div onClick={onClickMaking} className="modebutton" >
+<div className="modebuttonbox">
+    <div onClick={onClickMaking} className="modebutton" style={{marginRight:"auto"}}>
     <div className="buttonText">
       <div className="buttonT1">select mode</div>
-      <div className="buttonT2">주관식</div>
-      </div>
-      </div>
-    </li>
-  <li className="li">
-  <div onClick={onClickAdv} className="modebutton" >
-    <div className="buttonText">
-    <div className="buttonT1">typing mode</div>
       <div className="buttonT2">객관식</div>
       </div>
       </div>
-  </li>
-  </ul>
+  <div onClick={onClickAdv} className="modebutton" style={{marginLeft:"auto"}}>
+    <div className="buttonText">
+    <div className="buttonT1">typing mode</div>
+      <div className="buttonT2">주관식</div>
+      </div>
+      </div>
 </div>
 </div>
 </div>
