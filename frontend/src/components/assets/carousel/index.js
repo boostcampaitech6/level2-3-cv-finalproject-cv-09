02a7a22a14bd606bccf914735985362bf5807d77
jhd <wlgusehd0@gmail.com>
2024-03-19 03:55:14 +0000

    Feat: add slideshow on the Home page
    #41

---
+++ frontend/src/components/assets/carousel/index.js
@@ -0,0 +1,25 @@
+import boostcamp from './boostcamp.jpeg'
+import pinkbee from './pinkbee.jpg'
+import scv from './scv.jpg'
+
+const imageData = [
+    
+    {
+      label: "boostcamp",
+      alt: "image1",
+      src: boostcamp,
+    },
+  
+    {
+      label: "pinkbee",
+      alt: "image2",
+      src: pinkbee,
+    },
+  
+    {
+      label: "scv",
+      alt: "image3",
+      src: scv,
+    },
+]
+export default imageData;
