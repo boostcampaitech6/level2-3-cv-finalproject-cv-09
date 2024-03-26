30bf327d2b91ac3a7b6789af33973bc183620550
sangeon <qkr99102@naver.com>
2024-03-26 19:36:16 +0900

    Fix: start code init
    #62

--- backend/celery/start.sh
+++ backend/celery/start.sh
@@ -1,1 +1,4 @@
-poetry run celery -A worker_scv worker --loglevel=info --pool=solo
+nohup poetry run celery -A worker_deepfloyd worker -Q DeepFloyd_queue --loglevel=info --pool=solo &
+nohup poetry run celery -A worker_td worker -Q TD_queue --loglevel=info --pool=solo &
+nohup poetry run celery -A worker_sd2 worker -Q SD2_queue --loglevel=info --pool=solo &
+nohup poetry run celery -A worker_sdxl worker -Q SDXL_queue --loglevel=info --pool=solo &
